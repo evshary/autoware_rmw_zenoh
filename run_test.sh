0653c07b0db7d752e3a15282e86600d3c85313aa
ChenYing Kuo <evshary@gmail.com>
2025-03-10 13:53:21 +0800

    Add more ways to run test.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,15 @@ export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 source /opt/ros/humble/setup.bash
 source autoware/install/setup.bash
 cd autoware || exit
-colcon test
+
+#colcon test
+# Run test sequentially
+colcon test --executor sequential --event-handlers console_direct+
+# Run test for specific package, e.g., rclpy
+#colcon test --packages-select rclpy --event-handlers console_direct+
+# If you want to run a specific test 10 times until fail (The test ieme "test_controller" in the package "opennav_docking")
+#colcon test --packages-select opennav_docking --ctest-args -R test_controller --event-handlers console_direct+ --retest-until-fail 10
+
 cd - || exit
 
 # Collect result
