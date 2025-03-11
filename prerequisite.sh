06d3ed0c5f87d3742f4659d0e7d859c389d4e366
habby <a0979580915@gmail.com>
2025-03-11 18:19:45 +0800

    Build and run rmw_zenoh from source instead of binary

--- prerequisite.sh
+++ prerequisite.sh
@@ -5,5 +5,3 @@ set -e
 sudo apt install -y shellcheck pipx
 pipx install ruff
 pipx install pre-commit
-
-sudo apt install -y ros-humble-rmw-zenoh-cpp
--- run_zenohd.sh
+++ run_zenohd.sh
@@ -6,4 +6,5 @@ LOG_PATH=zenoh_log/zenohd/$(date '+%Y-%m-%d_%H:%M:%S')/
 mkdir -p "${LOG_PATH}"
 
 source /opt/ros/humble/setup.bash
+source rmw_zenoh/install/setup.bash
 RUST_LOG=z=debug ros2 run rmw_zenoh_cpp rmw_zenohd 2>&1 | tee "${LOG_PATH}"/zenohd.log
