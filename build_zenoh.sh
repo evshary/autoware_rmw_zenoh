598d9cf15b7f18be4a672c774aa5b392aa6f511e
habby <a0979580915@gmail.com>
2025-03-11 21:43:18 +0800

    ensure correct directory navigation

--- build_zenoh.sh
+++ build_zenoh.sh
@@ -6,7 +6,9 @@ if [ ! -d "rmw_zenoh" ]; then
     git clone https://github.com/ros2/rmw_zenoh.git -b humble
     cd .. || exit
     rosdep install --from-paths src --ignore-src --rosdistro humble -y
+    cd .. || exit
 fi
 
+cd rmw_zenoh || exit
 source /opt/ros/humble/setup.bash
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
