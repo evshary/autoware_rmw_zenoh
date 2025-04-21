ae72d00e20c97d1384f89358aea9d4d3cf813532
Leann Hsu <leann9001@gmail.com>
2025-04-21 11:00:19 +0800

    Apply composable node thread limit patch

--- build_autoware.sh
+++ build_autoware.sh
@@ -5,11 +5,17 @@ source /opt/ros/humble/setup.bash
 if [ ! -d "autoware" ]; then
     git clone https://github.com/autowarefoundation/autoware.git
     cd autoware || exit
+    git checkout d09350cf41242b86dce13f100267cb197cbf41f4
     mkdir src
     vcs import src < autoware.repos
     rosdep update
     rosdep install -r -y --from-paths src --ignore-src --rosdistro "$ROS_DISTRO"
     cd .. || exit
+
+    # Apply composable node thread limit patch
+    cd autoware/src/universe/autoware.universe || exit
+    git apply ../../../../patch/thread_limit.patch
+    cd ../../.. || exit
 fi
 
 cd autoware || exit
