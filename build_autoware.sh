eb911615ab89a2b7fcb596cadc8ccecb1c21be16
Leann Hsu <leann9001@gmail.com>
2025-04-21 14:19:57 +0800

    Add thread limit patch to avoid GuardCondition issue in Humble (#8)
    * Fix container script name
    
    * Source zenoh in test script
    
    * Apply composable node thread limit patch
    
    * Add explanation comment for thread limit patch (GuardCondition issue)
    
    * Add date for checked-out Autoware commit (2025-02-26)

--- build_autoware.sh
+++ build_autoware.sh
@@ -5,11 +5,23 @@ source /opt/ros/humble/setup.bash
 if [ ! -d "autoware" ]; then
     git clone https://github.com/autowarefoundation/autoware.git
     cd autoware || exit
+    git checkout d09350cf41242b86dce13f100267cb197cbf41f4 # 2025-02-26
     mkdir src
     vcs import src < autoware.repos
     rosdep update
     rosdep install -r -y --from-paths src --ignore-src --rosdistro "$ROS_DISTRO"
     cd .. || exit
+
+    # This patch runs behavior_path_planner in a separate container with one thread
+    # to avoid a GuardCondition use-after-free issue when using rmw_zenoh with multi-threaded executor.
+    # Known issue in ROS 2 Humble; fixed in Rolling (2025.04+), not backported.
+    #
+    # Related issues:
+    # - https://github.com/ros2/rmw_zenoh/issues/608
+    # - https://github.com/ros2/rclcpp/issues/2820
+    cd autoware/src/universe/autoware.universe || exit
+    git apply ../../../../patch/thread_limit.patch
+    cd ../../.. || exit
 fi
 
 cd autoware || exit
